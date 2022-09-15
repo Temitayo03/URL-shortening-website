615d55ab0d82860e21e6fad55b17c85d9ff7ea17
Temitayo03 <makanjuolac137@gmail.com>
2022-09-15 07:35:29 -0700

    setup project for vercel deployment

--- api/index.js
+++ api/index.js
@@ -1,38 +1,37 @@
 const express = require('express');
 const generateSlug = require('./middlewares/generateSlug')
 const path = require('path');
-const fs = require('fs/promises');
-const { readFile } = require('fs');
 
-const port = 9000;
+
+// const port = 9000;
 
 const app = express();
 // const router = express.Router();
 // const bodyParser = require("body-parser");
 
 app.use(express.urlencoded({extended: true}));
-app.use(express.static ('public'))
+app.use(express.static (path.join(__dirname, "..", 'public')))
 app.use(express.json());
 // app.use("/", router)
 // app.use(bodyParser());
-
+const data = [];
 
 app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname,"/urls.html"))
+    res.sendFile(path.join(__dirname,"..","urls.html"))
 });
 
 app.get('/:slug', async(req, res)=>{
     // read from the data.json 
     let sluglinks
-    try {
+    // try {
         
-        sluglinks = await fs.readFile(path.resolve('data.json'), 'utf8');
-        sluglinks = JSON.parse(sluglinks);
-    } catch (error) {
-        sluglinks = [];
-    }
+    //     sluglinks = await fs.readFile(path.resolve('data.json'), 'utf8');
+    //     sluglinks = JSON.parse(sluglinks);
+    // } catch (error) {
+    //     sluglinks = [];
+    // }
 
-    let match = sluglinks.find((item)=>{
+    let match = data.find((item)=>{
         return item.slug === req.params.slug
     });
 
@@ -46,23 +45,24 @@ app.get('/:slug', async(req, res)=>{
 
 app.post('/short',generateSlug, async (req, res)=>{
     // open data.json to retrieve former data
-    let data;
-    try {
-        let data = await fs.readFile(path.resolve('data.json'), 'r', 'utf8');
-        data = JSON.parse(data);
-    } catch (error) {
-        data = [];
-    }
+    // let data;
+    // try {
+    //     let data = await fs.readFile(path.resolve('data.json'), 'r', 'utf8');
+    //     data = JSON.parse(data);
+    // } catch (error) {
+    //     data = [];
+    // }
     // create the new url object
+
     let link = {
         originalURL: req.body.linkShorten,
         slug: req.slug,
         shortURL: `${req.protocol}://${req.get('host')}/${req.slug}`
     }
     data.push(link);
-    await fs.writeFile(path.resolve('data.json'), JSON.stringify(data),'utf8')
-    console.log(req.body);
-    console.log(req.slug);
+    // await fs.writeFile(path.resolve('data.json'), JSON.stringify(data),'utf8')
+    // console.log(req.body);
+    // console.log(req.slug);
     res.status(200).json(link);
     
 
@@ -74,8 +74,8 @@ app.post('/short',generateSlug, async (req, res)=>{
 //     res.sendFile(path.resolve('urls.html'))
 // })
 
-app.listen(port, ()=>{
-    console.log(`server is running on http://localhost:${port}`)
-})
+// app.listen(port, ()=>{
+//     console.log(`server is running on http://localhost:${port}`)
+// })
 
 
